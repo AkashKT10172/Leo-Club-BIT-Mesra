54ec957cbcc3090290ff545e4df6639f9785818e
Rishavsingh03 <rishavkumar3864@gmail.com>
2024-05-22 15:41:16 +0530

    Created basic About page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './AboutUs.css'
 const AboutUs = () => {
   return (
-    <div className='h-screen flex justify-center items-center'>About Us page coming soon....</div>
+    <div className='Acontainer'>
+      <h1 className='Aheader bg-gradient-to-r from-blue-800 via-blue-900 to-blue-800 text-transparent bg-clip-text'>About Us</h1>
+      <img className='logo' src='./leoLogo.png'></img>
+      <div className="Acontent">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      orem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod magna at laoreet rutrum. Donec sed odio dui. Fusce dapibus, tellus quis lacinia congue, erat lorem pulvinar nunc, condimentum feugiat nisl purus pretium nulla.
+      </p>
+    </div>
+    </div>
   )
 }
 

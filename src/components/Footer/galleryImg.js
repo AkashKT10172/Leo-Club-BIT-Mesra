c555cd5aaab8f6457e6f5e6b88ae457b924a2a82
Akash Kumar Tiwary <btech10172.22@bitmesra.ac.in>
2024-05-27 03:37:59 +0530

    please deploy ni hora ;(

--- src/components/Footer/galleryImg.js
+++ src/components/Footer/galleryImg.js
@@ -1,9 +1,9 @@
-import image1 from '/src/components/Gallery/assets/Deepotsav.png'
-import image2 from '/src/components/Gallery/assets/img1.jpg'
-import image3 from '/src/components/Gallery/assets/img2.jpg'
-import image4 from '/src/components/Gallery/assets/img3.jpg'
-import image5 from '/src/components/Gallery/assets/img4.jpg'
-import image6 from '/src/components/Gallery/assets/SquidGame.png'
+import image1 from '/src/components/Footer/assets/Deepotsav.png'
+import image2 from '/src/components/Footer/assets/img1.jpg'
+import image3 from '/src/components/Footer/assets/img2.jpg'
+import image4 from '/src/components/Footer/assets/img3.jpg'
+import image5 from '/src/components/Footer/assets/img4.jpg'
+import image6 from '/src/components/Footer/assets/SquidGame.png'
 
 export default {
     images : [

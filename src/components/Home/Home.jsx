4e972b675620de3940ca98017c5d441465c1b932
Rishavsingh03 <rishavkumar3864@gmail.com>
2024-06-18 09:32:48 +0530

    added animation in info div home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,33 @@ import TeamDiv from './TeamDiv/TeamDiv.jsx'
 import Blog from './Blog/Blog.jsx'
 import President from './President/President.jsx'
 import Contact from './ContactUs/Contact.jsx'
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 function Home() {
+  gsap.registerPlugin(useGSAP);
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(()=>{
+    let t1_1=gsap.timeline({
+      scrollTrigger:{
+        trigger:".gintro",
+        start:"top center",
+        markers:"true"
+      },
+    });
+
+    t1_1.from(".glogo-image",{xPercent:-180,duration:1});
+    t1_1.from(".gmotto",{y:20,duration:1});
+    t1_1.from(".gmotto",{opacity:0,duration:1.5},1);  
+
+    t1_1.from(".ginfo-head",{xPercent:200,duration:1},0);
+    t1_1.from(".ginfo-content",{scale:0,duration:1.5},1);
+
+    t1_1.from(".gknow-more",{y:-50,duration:1,ease:"bounce.out"},1.5);
+    t1_1.from(".gknow-more",{opacity:0,duration:1},1.5);
+
+
+  })
   return (
     <>
     <Video />
--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -6,18 +6,18 @@ import {handleClick} from "../../onClick"
 
 function IntroDiv() {
     return(
-        <div className ="main-div">
+        <div className ="main-div gintro overflow-x-hidden">
             <div className="image-div">
-                <img src={leoLogo} alt="ok"/>
-                <h1 className="text-xl text-gray-700 font-black text-center flex flex-wrap justify-center align-center">
+                <img src={leoLogo} alt="ok" className="glogo-image"/>
+                <h1 className="text-xl text-gray-700 font-black text-center flex flex-wrap justify-center align-center gmotto">
                 <span className="bg-gradient-to-r from-red-500 via-pink-500 to-blue-500 text-transparent bg-clip-text">LEADERSHIP&nbsp;&nbsp;</span>
                 <span className="bg-gradient-to-r from-red-500 via-pink-500 to-blue-500 text-transparent bg-clip-text">EXPERIENCE&nbsp;&nbsp;</span>
                 <span className="bg-gradient-to-r from-red-500 via-pink-500 to-blue-500 text-transparent bg-clip-text">OPPURTUNITY</span>
                 </h1>
             </div>
             <div className="info-div">
-                <h1 className="text-3xl text-gray-700 font-black text-center">LEO CLUB BIT MESRA</h1>
-                <p className="px-10 py-10 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                <h1 className="text-3xl text-gray-700 font-black text-center ginfo-head">LEO CLUB BIT MESRA</h1>
+                <p className="px-10 py-10 text-center ginfo-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Voluptatibus eligendi enim, cum minus reiciendis hic aliquid dolorem, qui repudiandae aspernatur
                 vero voluptas, facilis repellendus nesciunt commodi molestiae delectus quam possimus?Lorem ipsum,
                 dolor sit amet consectetur adipisicing elit. Explicabo nihil quisquam, dolores eos minima optio
@@ -25,7 +25,7 @@ function IntroDiv() {
                 quas.
                 </p>
                
-                <button className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded gknow-more">
                     <NavLink to="/about" onClick={handleClick}>Click to know more!</NavLink>
                 </button>
              

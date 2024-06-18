5fe772682fc968dabb90ab20888073772c5ca582
Akash Kumar Tiwary <btech10172.22@bitmesra.ac.in>
2024-06-18 16:33:04 +0530

    added team members

--- src/components/Team/seniors.js
+++ src/components/Team/seniors.js
@@ -3,57 +3,68 @@ import sakshi from "/src/components/Team/imagesTeam/Sakshi.png"
 import mayank from "/src/components/Team/imagesTeam/Mayank.jpg"
 import akhil from "/src/components/Team/imagesTeam/Akhil.jpg"
 import aaheli from "/src/components/Team/imagesTeam/Aaheli.jpg"
+import aqsa from "/src/components/Team/imagesTeam/Aqsa.jpg"
+import radhika from "/src/components/Team/imagesTeam/Radhika.jpg"
+import aditi from "/src/components/Team/imagesTeam/Aditi.jpeg"
+import shashwat from "/src/components/Team/imagesTeam/Shashwat.jpg"
 
 export default {
     seniorMembers : [
-        {   image : akash,
-            name : "Akash Kumar Tiwary",
-            designation : "Webmaster",
-            instagram : "akashdidwhat",
-            linkedin : "akashkt10172",
-            mail : "tweetsbyakash"
+        {   image : shashwat,
+            name : "Shashwat Jha",
+            designation : "President",
+            instagram : "saucewhat_",
+            linkedin : "shashwatjha951",
+            mail : ""
         },
         {   image : sakshi,
             name : "Sakshi Kumari",
             designation : "Joint President",
             instagram : "__areee_yaaarrr_sakshiii__",
             linkedin : "sakshi-sharma-477363229",
-            mail : ""
+            mail : "btech10457.21@bitmesra.ac.in"
         },
         {   image : akhil,
             name : "Akhilanand Singh",
             designation : "Vice President",
             instagram : "chauhan.akhilanand",
             linkedin : "akhilanand-singh-20a340228",
-            mail : ""
+            mail : "akhilanandsingh5@gmail.com"
         },
-        {   image : akash,
-            name : "Akash Kumar Tiwary",
-            designation : "Webmaster",
-            instagram : "akashdidwhat",
-            linkedin : "akashkt10172",
-            mail : "tweetsbyakash"
+        {   image : aqsa,
+            name : "Aqsa Ayub",
+            designation : "Vice President ",
+            instagram : "ayub_aqsa",
+            linkedin : "",
+            mail : "aqsaayub8514@gmail.com"
+        },
+        {   image : radhika,
+            name : "Radhika Rajesh Gandhi",
+            designation : "Director",
+            instagram : "raadhika._.gaandhi",
+            linkedin : "",
+            mail : "btech10755.21@bitmesra.ac.in"
         },
         {   image : mayank,
             name : "Mayank Giri",
             designation : "Co-Director",
             instagram : "mayankgiri2110",
             linkedin : "mayank-giri-66b24321b",
-            mail : ""
+            mail : "mayankgiri708@gmail.com"
         },
         {   image : aaheli,
             name : "Aaheli Biswas",
             designation : "Senior Events Head",
             instagram : "Aaheli_102",
             linkedin : "aaheli-biswas-87b647251",
-            mail : ""
+            mail : "btech10804.21@bitmesra.ac.in"
         },
-        {   image :akash,
-            name : "Akash Kumar Tiwary",
-            designation : "Webmaster",
-            instagram : "akashdidwhat",
-            linkedin : "akashkt10172",
-            mail : "tweetsbyakash"
+        {   image :aditi,
+            name : "Aditi",
+            designation : "Senior Executive Member ",
+            instagram : "aditi.1703",
+            linkedin : "aditi-b19829225",
+            mail : "btech10243.21@bitmesra.ac.in"
         }     
     ]
 };

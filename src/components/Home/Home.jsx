ec61d8a9885b6abe2c27ecf04114028c254be2d3
Rishavsingh03 <rishavkumar3864@gmail.com>
2024-06-18 20:07:28 +0530

    modified the animations on home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,69 +1,78 @@
-import React from 'react'
-import Video from './Bg_video/Bg_video.jsx'
-import IntroDiv from './Intro/Intro.jsx'
-import Event from './Event/Event.jsx'
-import TeamDiv from './TeamDiv/TeamDiv.jsx'
-import Blog from './Blog/Blog.jsx'
-import President from './President/President.jsx'
-import Contact from './ContactUs/Contact.jsx'
+import React, { useEffect } from 'react';
+import Video from './Bg_video/Bg_video.jsx';
+import IntroDiv from './Intro/Intro.jsx';
+import Event from './Event/Event.jsx';
+import TeamDiv from './TeamDiv/TeamDiv.jsx';
+import Blog from './Blog/Blog.jsx';
+import President from './President/President.jsx';
+import Contact from './ContactUs/Contact.jsx';
 import gsap from 'gsap';
-import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { startAt } from 'firebase/firestore';
+
+gsap.registerPlugin(ScrollTrigger);
+
 function Home() {
-  gsap.registerPlugin(useGSAP);
-  gsap.registerPlugin(ScrollTrigger);
-  useGSAP(()=>{
-    let t1_1=gsap.timeline({
-      scrollTrigger:{
-        trigger:".gintro",
-        start:"top center",
-      },
+  useEffect(() => {
+    const introElements = [
+      { trigger: '.glogo-image', animation: { xPercent: -180, duration: 1 } },
+      { trigger: '.gmotto', animation: { y: 20, opacity: 0, duration: 1.5 } },
+      { trigger: '.ginfo-head', animation: { xPercent: 200, duration: 1 } },
+      { trigger: '.ginfo-content', animation: { scale: 0, duration: 1.5 } },
+      { trigger: '.gknow-more', animation: { y: -50, opacity: 0, duration: 1, ease: 'bounce.out' } },
+    ];
+
+    const eventElements = [
+      { trigger: '.gevent-heading', animation: { scale: 0, duration: 1 } },
+      { trigger: '.gevent-img', animation: { x: 150, opacity: 0, duration: 1.5 } },
+      { trigger: '.gevent-info', animation: { y: 150, opacity: 0, duration: 1.5 } },
+      { trigger: '.gmore-event', animation: { y: -100, opacity: 0, duration: 1, ease: 'bounce.out' } },
+    ];
+
+    // Function to create animations for elements
+    const createAnimations = (elements, start) => {
+      elements.forEach((el) => {
+        gsap.from(el.trigger, {
+          ...el.animation,
+          scrollTrigger: {
+            trigger: el.trigger,
+            start:el.trigger==".gmotto"||".gmore-event"?"top 85%":start,
+            toggleActions: 'play pause resume reset',
+          },
+        });
+      });
+    };
+
+    // Media queries for responsive animations
+    const mm = gsap.matchMedia();
+
+    mm.add("(min-width: 768px)", () => {
+      createAnimations(introElements, "top 60%");
+      createAnimations(eventElements, "top 70%");
     });
 
-    let t1_2=gsap.timeline({
-      scrollTrigger:{
-        trigger:".gevent",
-        start:"top 98%",
-      },
+    mm.add("(max-width: 767px)", () => {
+      createAnimations(introElements, "top 80%");
+      createAnimations(eventElements, "top 80%");
     });
 
-    t1_1.from(".glogo-image",{xPercent:-180,duration:1});
-    t1_1.from(".gmotto",{y:20,duration:1});
-    t1_1.from(".gmotto",{opacity:0,duration:1.5},1);  
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+      mm.kill();
+    };
+  }, []);
 
-    t1_1.from(".ginfo-head",{xPercent:200,duration:1},0);
-    t1_1.from(".ginfo-content",{scale:0,duration:1.5},1);
-
-    t1_1.from(".gknow-more",{y:-100,duration:1,ease:"bounce.out"},2);
-    t1_1.from(".gknow-more",{opacity:0,duration:1},2.5);
-
-    t1_2.from(".gevent",{y:200,duration:1});
-    t1_2.from(".gevent",{opacity:0,duration:1},0);
-    t1_2.from(".gevent-heading",{scale:0,duration:0.5},1);
-    t1_2.from(".gevent-info",{y:150,duration:1.5},1.5);
-    t1_2.from(".gevent-info",{opacity:0,duration:1},1.5);
-    t1_2.from(".gevent-img",{x:150,duration:1.5},1.5)
-    t1_2.from(".gevent-img",{opacity:0,duration:1},1.5);
-
-    t1_2.from(".gmore-event",{y:-100,duration:1,ease:"bounce.inOut"},2);
-    t1_2.from(".gmore-event",{opacity:0,duration:1},2.5);
-
-
-  ;
-
-
-  })
   return (
     <>
-    <Video />
-    <IntroDiv />
-    <Event />
-    <TeamDiv />
-    <Blog />
-    {/* <President /> */}
-    <Contact />
+      <Video />
+      <IntroDiv />
+      <Event />
+      <TeamDiv />
+      <Blog />
+      {/* <President /> */}
+      <Contact />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;

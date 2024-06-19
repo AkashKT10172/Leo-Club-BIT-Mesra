ba763bd5cb9db9c40ad0e95fe4ae13b76f8a2010
Rishavsingh03 <rishavkumar3864@gmail.com>
2024-06-19 12:37:55 +0530

    Modified Gallery Page

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import gsap from 'gsap';
 import mm1 from './assets/mm1.jpg';
 import mm2 from './assets/mm2.jpg';
 import mm3 from './assets/mm3.jpg';
@@ -21,27 +22,54 @@ import dd12 from './assets/dd12.jpg';
 const images=[dd1,dd2,dd3,dd4,dd5,dd6,dd7,dd8,dd9,dd10,dd11,dd12,mm1,mm2,mm3,mm4,mm5,mm6,mm7];
 
 const Gallery = () => {
+  const [isLoading, setIsLoading] = useState(true); 
   const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    const scrollTrigger = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.grid-container', 
+        start: 'top bottom', 
+        end: 'bottom top',
+      },
+    });
+
+    gridItems.forEach((item) => {
+      scrollTrigger.fromTo(
+        item,
+        { opacity: 0, scaleX: 0 }, 
+        { opacity: 1, scaleX: 1, duration: 0.5 }, 
+        '<0.2' 
+      );
+    });
+
+    scrollTrigger.play(); 
+  }, []); 
 
   const handleImageClick = (image) => {
     setSelectedImage(image);
+    document.body.classList.add('overflow-hidden'); 
   };
 
   const handleCloseClick = () => {
     setSelectedImage(null);
+    document.body.classList.remove('overflow-hidden'); 
   };
 
   return (
     <div className="h-full w-full flex flex-col justify-center items-center mb-6 mt-16">
-      <h1 className="text-4xl font-black bg-gradient-to-r from-blue-800 via-blue-900 to-blue-800 text-transparent bg-clip-text my-4 p-1 text-center">IMAGE GALLERY</h1>
-      <div className="flex flex-wrap justify-evenly items-center  gap-1 w-11/12  sm:w-4/5  md:gap-4 h-full md:w-4/5">
+      <h1 className="text-4xl font-black bg-gradient-to-r from-blue-800 via-blue-900 to-blue-800 text-transparent bg-clip-text my-4 p-1 text-center">
+        IMAGE GALLERY
+      </h1>
+      <div className="w-11/12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 grid-container">
         {images.map((image, index) => (
-          <div key={index} className="card group h-58 w-full sm:h-64 sm:w-5/12 md:h-80 md:w-1/4">
+          <div key={index} className="relative overflow-hidden max-h-[300px] rounded-md grid-item"> 
             <img
               src={image}
-              loading='lazy'
+              loading="lazy"
               alt={`Image ${index + 1}`}
-              className="card-image object-cover  h-full w-full shadow-xl rounded-sm  sm:hover:scale-105 sm:hover:opacity-80 cursor-pointer transition-transform  duration-300"
+              className="card-image object-cover h-full w-full shadow-xl rounded-sm sm:hover:scale-105 sm:hover:opacity-80 cursor-pointer transition-transform duration-300"
               onClick={() => handleImageClick(image)}
             />
           </div>
@@ -52,12 +80,12 @@ const Gallery = () => {
           <img
             src={selectedImage}
             alt="Selected Image"
-            className="w-full h-[450px] rounded-lg max-w-3xl max-h-full object-contain hover:cursor-pointer  mx-auto"
+            className="w-full h-[450px] rounded-lg max-w-3xl max-h-full object-contain hover:cursor-pointer mx-auto"
             onClick={handleCloseClick}
           />
           <button
             type="button"
-            className="absolute top-0 right-0 m-4 text-white  hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-full p-2 shadow-sm"
+            className="absolute top-0 right-0 m-4 text-white hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-full p-2 shadow-sm"
             onClick={handleCloseClick}
           >
             X

bc699c256c48549a62fe92b61891db1ebbe0cc04
Rishavsingh03 <rishavkumar3864@gmail.com>
2024-06-25 22:20:19 +0530

    added rishav details

--- src/components/Team/juniors.js
+++ src/components/Team/juniors.js
@@ -10,16 +10,17 @@ import anjan from "/src/components/Team/imagesTeam/Anjan.jpg"
 import mohit from "/src/components/Team/imagesTeam/Mohit.jpg"
 import tanuja from "/src/components/Team/imagesTeam/Tanuja.jpg"
 import pranav from "/src/components/Team/imagesTeam/Pranav.jpg"
+import rishav from '/src/components/Team/imagesTeam/Rishav.jpg'
 
 
 export default {
     juniorMembers: [
-        {   image : akash,
+        {   image : rishav,
             name : "Rishav Kumar",
             designation : "General Secretary",
-            instagram : "akashdidwhat",
-            linkedin : "null",
-            mail : ""
+            instagram : "__rishavsingh12",
+            linkedin : "rishav-kumar-7089732b0",
+            mail : "rishavkumar3864@gmail.com"
         },
         {   image : khushi,
             name : "Khushi Sharma",
